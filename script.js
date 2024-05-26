1fccef39f9dda28a0b868d6839075e51fcaa75a6
Taylan <ferreirataylan123@gmail.com>
2024-05-26 13:30:56 -0300

    create validations

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const addressWarn = document.getElementById("address-warn");
 let cart = [];
 
 cartBtn.addEventListener("click", function () {
+	updateCartModal();
 	cartModal.style.display = "flex";
 });
 
@@ -40,7 +41,6 @@ function addToCart(name, price) {
 
 	if (existingItem) {
 		existingItem.quantity += 1;
-		return;
 	} else {
 		cart.push({
 			name,
@@ -52,4 +52,128 @@ function addToCart(name, price) {
 	updateCartModal();
 }
 
-function updateCartModal() {}
+function updateCartModal() {
+	cartItemsContainer.innerHTML = "";
+
+	let total = 0;
+
+	cart.forEach((item) => {
+		const cartItemElement = document.createElement("div");
+		cartItemElement.classList.add(
+			"flex",
+			"justify-between",
+			"mb-4",
+			"flex-col"
+		);
+
+		cartItemElement.innerHTML = `
+			<div class='flex items-center justify-between' > 
+				<div> 
+					<p class='font-medium' > ${item.name} </p>
+					<p> Qtd: ${item.quantity} </p>
+					<p class='font-medium mt-2' > ${item.price.toFixed(2)} </p>
+				</div>
+
+				<button class='remove-btn' data-name='${item.name}' >
+					Remover 
+				</button>
+			</div>
+		`;
+
+		total += item.price * item.quantity;
+
+		cartItemsContainer.appendChild(cartItemElement);
+	});
+
+	cartTotal.textContent = total.toLocaleString("pt-BR", {
+		style: "currency",
+		currency: "BRL",
+	});
+
+	cartCounter.innerHTML = cart.length;
+}
+
+cartItemsContainer.addEventListener("click", function (event) {
+	if (event.target.classList.contains("remove-btn")) {
+		const name = event.target.getAttribute("data-name");
+
+		removeItemCart(name);
+	}
+});
+
+function removeItemCart(name) {
+	const index = cart.findIndex((item) => item.name === name);
+
+	if (index !== -1) {
+		const item = cart[index];
+		console.log(item);
+
+		if (item.quantity > 1) {
+			item.quantity -= 1;
+			updateCartModal();
+			return;
+		}
+
+		cart.splice(index, 1);
+		updateCartModal();
+	}
+}
+
+addressInput.addEventListener("input", function (event) {
+	let inputValue = event.target.value;
+
+	if (inputValue !== "") {
+		addressInput.classList.remove("border-red-500");
+		addressWarn.classList.add("hidden");
+	}
+});
+
+checkoutBtn.addEventListener("click", function () {
+	const isOpen = checkRestaurantOpen();
+
+	if (!isOpen) {
+		alert("O RESTAURANTE ESTÁ FECHADO NO MOMENTO!");
+		return;
+	}
+
+	if (cart.length === 0) return;
+	if (addressInput.value === "") {
+		addressWarn.classList.remove("hidden");
+		addressInput.classList.add("border-red-500");
+		return;
+	}
+
+	const cartItems = cart
+		.map((item) => {
+			return ` ${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price} |`;
+		})
+		.join();
+
+	const massage = encodeURIComponent(cartItems);
+	const phone = "+5585987765818";
+
+	window.open(
+		`https://wa.me/${phone}?text=${massage} Endereço: ${addressInput.value}`,
+		"_blank"
+	);
+
+	cart = [];
+	updateCartModal();
+});
+
+function checkRestaurantOpen() {
+	const data = new Date();
+	const hora = data.getHours();
+	return hora > 18 && hora > 22;
+}
+
+const spanItem = document.getElementById("date-span");
+const isOpen = checkRestaurantOpen();
+
+if (isOpen) {
+	spanItem.classList.remove("bg-red-500");
+	spanItem.classList.add("bg-green-600");
+} else {
+	spanItem.classList.remove("bg-green-600");
+	spanItem.classList.add("bg-red-500");
+}
