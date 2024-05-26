2b5e32cc777fc714026f55631126e89f99349ad2
Taylan <ferreirataylan123@gmail.com>
2024-05-26 13:37:32 -0300

    add toast message

--- script.js
+++ script.js
@@ -132,7 +132,18 @@ checkoutBtn.addEventListener("click", function () {
 	const isOpen = checkRestaurantOpen();
 
 	if (!isOpen) {
-		alert("O RESTAURANTE ESTÁ FECHADO NO MOMENTO!");
+		Toastify({
+			text: "Ops, o restaurante está fechado!",
+			duration: 3000,
+			close: true,
+			gravity: "top", // `top` or `bottom`
+			position: "right", // `left`, `center` or `right`
+			stopOnFocus: true, // Prevents dismissing of toast on hover
+			style: {
+				background: "#ef4444",
+			},
+		}).showToast();
+
 		return;
 	}
 
